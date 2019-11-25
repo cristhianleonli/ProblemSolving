629e72ceea281860a2354edbfdb67ca9be918951
Cristhian Leon <cris.leonardoleon@gmail.com>
2019-11-25 06:18:38 +0100

    Added reverse

---
+++ reverse.exs
@@ -0,0 +1,37 @@
+defmodule Reader do
+  def read(type \\ :string) do
+    case type do
+      :str -> read_line()
+      :int -> read_line() |> String.to_integer()
+    end
+  end
+
+  defp read_line() do
+    IO.read(:stdio, :line) |> String.replace("\n", "")
+  end
+
+  def read_eof(prev) do
+    case IO.read(:stdio, :line) do
+      :eof ->
+        prev
+
+      {:error, reason} ->
+        IO.puts("Error: #{reason}")
+
+      data ->
+        string = data |> String.replace("\n", "")
+        read_eof(prev ++ [string])
+    end
+  end
+end
+
+defmodule Solution do
+  def filter(list) do
+    list
+    |> Enum.reverse()
+    |> Enum.each(&IO.puts(&1))
+  end
+end
+
+data = Reader.read_eof([])
+Solution.filter(data)
