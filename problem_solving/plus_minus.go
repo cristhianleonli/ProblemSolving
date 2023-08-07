51cb62a5bd24a6f5c42483350db24fe867268b98
Cristhian Leon <cris.leonardoleon@gmail.com>
2023-08-07 07:24:55 -0500

    Added plus minus

--- problem_solving/plus_minus.go
+++ problem_solving/plus_minus.go
@@ -3,9 +3,25 @@ package main
 import "fmt"
 
 func plusMinus(arr []int32) {
-	fmt.Println(arr)
+	weightPerElement := float32(1) / float32(len(arr))
+
+	var negatives float32 = 0
+	var positives float32 = 0
+	var zeros float32 = 0
+
+	for _, number := range arr {
+		if number == 0 {
+			zeros += weightPerElement
+		} else if number > 0 {
+			positives += weightPerElement
+		} else {
+			negatives += weightPerElement
+		}
+	}
+
+	fmt.Printf("%.6f\n%.6f\n%.6f", positives, negatives, zeros)
 }
 
 func main() {
-	plusMinus([]int32{1, 1, 0, -1, -1})
+	plusMinus([]int32{-4, 3, -9, 0, 4, 1})
 }
