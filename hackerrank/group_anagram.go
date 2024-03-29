2d65e34750e28a7ad8c5392e0e8d90a96fcf6254
Cristhian Leon <cris.leonardoleon@gmail.com>
2024-03-29 15:09:36 -0300

    Adds group anagrams

---
+++ hackerrank/group_anagram.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func main() {
+	items := []string{"cat", "tac", "bat", "tab"}
+	result := groupAnagrams(items)
+	fmt.Println(result)
+}
+
+func groupAnagrams(strs []string) [][]string {
+	m := map[string][]string{}
+
+	for _, word := range strs {
+		f := key(word)
+		p, ok := m[f]
+
+		if ok {
+			p = append(p, word)
+			m[f] = p
+		} else {
+			m[f] = []string{word}
+		}
+	}
+
+	r := [][]string{}
+	for _, v := range m {
+		r = append(r, v)
+	}
+
+	return r
+}
+
+func key(w string) string {
+	s := strings.Split(w, "")
+	sort.Strings(s)
+	return strings.Join(s, "")
+}
