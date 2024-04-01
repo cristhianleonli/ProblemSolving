0557c30f2e7266cefeefd4ffe70af5759c7484d7
Cristhian Leon <cris.leonardoleon@gmail.com>
2024-04-01 06:27:27 -0300

    majority element

--- leetcode/majority_element.go
+++ leetcode/majority_element.go
@@ -3,11 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	r := majorityElement([]int{2, 2, 1, 1, 1, 2, 2})
+	// r := majorityElement([]int{2, 2, 1, 1, 1, 2, 2})
+	r := majorityElement([]int{3, 2, 3})
 	fmt.Println(r)
 }
 
 func majorityElement(nums []int) int {
+	memo := make(map[int]int)
+	greatest := -1
+	greatest_count := -1
 
-	return 0
+	for _, n := range nums {
+		memo[n]++
+
+		if memo[n] > greatest_count {
+			greatest = n
+			greatest_count = memo[n]
+		}
+	}
+
+	return greatest
 }
